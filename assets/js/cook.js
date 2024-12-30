6b8f0684703a33c77da1a2a0b7c18e788292ccbd
JGabrielFDC <151442605+jgabrielfdc@users.noreply.github.com>
2024-12-30 15:51:47 -0300

    -Alteração de Sprites

--- assets/js/cook.js
+++ assets/js/cook.js
@@ -15,9 +15,21 @@ $(document).ready(function(){
 
     $("#fonte_carne").on("click",function(){
         if(temCarne()){
-                let carne=document.createElement("div")
-                $("#timer").html("00:05")
+
+
+                // Cria um elemento para representar a carne
+                let carne=document.createElement("img")
+
+                $(carne).addClass("tamanho_carne")
+                $(carne).css("cursor","pointer");
+                $(carne).attr("id","carne_crua");
+                $(carne).attr("src","assets/imgs/carne_crua.png");
+                $("#chapa").prepend($(carne));
+                
+                // Seta o timer inicial
                 let segundos=5;
+                $("#timer").html("00:0"+segundos)
+                
                     let timer=setInterval(function () {
                         if(segundos>1){
                             segundos-=1;
@@ -25,17 +37,16 @@ $(document).ready(function(){
                         }else{
                             $("#timer").html("00:00")
                             let idItemChapa=$("#chapa")[0].children[0].id="carne_assada";
+
                             if(temCarne() && $("#chapa")[0].children[0].id=="carne_crua"){
                                 $("#"+idItemChapa).attr("id","carne_assada");
                                 $("#timer").html("00:00");
                             }
+                            $("#carne_assada")[0].src="assets/imgs/carne_assada.png"; 
                             clearInterval(timer);
                         }
                     }, 1000);
-                $(carne).addClass("bg-danger rounded-circle tamanho_carne")
-                $(carne).css("cursor","pointer");
-                $(carne).attr("id","carne_crua");
-                $("#chapa").prepend($(carne));
+                
             }
     })
    
