edfe256c1b7cc35a1bcf558cbe6f90bbfd5aa49a
JGabrielFDC <151442605+jgabrielfdc@users.noreply.github.com>
2025-01-09 14:01:04 -0300

    -Lógica Entrega Pedidos

--- assets/js/serve.js
+++ assets/js/serve.js
@@ -1,8 +1,4 @@
 $(document).ready(function(){
-    let initClientes=setTimeout(function(){
-        let tempo
-    },tempoCliente);
-    clearTimeout(initClientes);
 
-
+        console.log(($(this)))
 })
