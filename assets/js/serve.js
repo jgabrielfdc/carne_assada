fc83dca0f677e531975e1e29170b19636a21703b
JGabrielFDC <151442605+jgabrielfdc@users.noreply.github.com>
2025-01-14 14:57:56 -0300

    :lipstick: feat: Lista de Pedidos

--- assets/js/serve.js
+++ assets/js/serve.js
@@ -1,13 +1,23 @@
 $(document).ready(function(){
-        let lista_clientes=[];
+        var lista_clientes=[];
         $("#clientes div ul").children().each(function(){
                 let id=$(this).parent().parent().attr('id')
                 let cliente='#'+id;
+                let h4=document.createElement("h4");
+                let li=document.createElement("li");
+                let ul=document.createElement("ul");
+                h4.innerHTML=id;
+                $(li).append(h4)              
+                $(li).append(ul)              
                 lista_clientes.push(id);
                 lista_clientes[id]=[];
+                
                 $(cliente+" ul").children().each(function(){
+                        
                         let pedido=$(this).attr("id");
                         lista_clientes[id].push(pedido);
                 })
+                $("#lista_pedidos").append(li);
        })
+       console.log(lista_clientes)
 })
